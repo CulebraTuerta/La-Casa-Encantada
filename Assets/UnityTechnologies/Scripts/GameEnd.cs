b401cd01cab7800021134eaf16dab2eceafa8a20
CulebraTuerta <d.marinaguayo@gmail.com>
2025-05-20 23:35:12 -0400

    dm

---
+++ Assets/UnityTechnologies/Scripts/GameEnd.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnd : MonoBehaviour
+{
+    public GameObject player; //con esto le decimos al objeto que le pongamos al script, que necesita interactuar con un objeto player
+    public CanvasGroup backgroundWin;
+    public CanvasGroup backgroundLose;
+    public float fadeDuration;
+    public float displayImageDuration; //nos da un poco mas de tiempo para terminar la app
+
+    public AudioSource winSound;
+    public AudioSource loseSound;
+    private bool hasAudioPlayed;
+
+    private float timer;
+    private bool isPlayer;
+    private bool isCaugth;
+    void Start()
+    {
+            
+    }
+
+    void Update()
+    {
+        if (isPlayer)
+        {
+            EndLevel(backgroundWin,false,winSound); //false porque no queremos que reinicie el nivel
+        }
+        else if(isCaugth)
+        {
+            EndLevel(backgroundLose,true,loseSound); //aqui el true es simplemente para repetir el nivel
+            CaughtPlayer();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) //si otro objeto entra en colision
+    {
+        if (other.gameObject==player)
+        {
+            isPlayer = true;
+        }
+    }
+
+    public void EndLevel(CanvasGroup imgCanvas,bool doRestart, AudioSource sound) //Con este canvasgroup puedo elegir cualquera de las dos situaciones.
+    {
+        if (!hasAudioPlayed) //si no esta sonando nada
+        {
+            sound.Play();
+            hasAudioPlayed = true;
+        }
+
+        timer += Time.deltaTime; //intervalo en segundos del ultimo frame al siguiente.
+        imgCanvas.alpha = timer/fadeDuration; //modificando el valor podemos determinar cuanto se demorara
+
+        if(timer>fadeDuration+displayImageDuration)
+        {
+            
+            if(doRestart)
+            {
+                //necesitamos recargar la misma escena donde estamos.., necesitaremos una libreria unityengine.scenemanagement;
+                SceneManager.LoadScene(0); //no  se por que el cero...
+            }
+            else
+            {
+                Application.Quit();  //esto cierra el juego, solo funciona en el juego exportado.
+            }
+        }
+    }
+
+    public void CaughtPlayer()
+    {
+        isCaugth= true;
+    }
+}
--- Assets/UnityTechnologies/Scripts/PlayerMovement.cs
+++ Assets/UnityTechnologies/Scripts/PlayerMovement.cs
@@ -4,43 +4,78 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float turnSpeed = 20f;
+    public float turnSpeed = 100f;
 
     Vector3 movement;
     Quaternion rotation = Quaternion.identity;
 
     Animator anim;
     Rigidbody rigid;
+
+    AudioSource audio;
     
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         rigid = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        movement.Set(horizontal, 0f, vertical);
+        movement.Set(0f, 0f, vertical);  // Solo movimiento adelante/atrás, sacamos el horizontal, asi con ese eje giramos al personaje
         movement.Normalize();
 
-        bool horizontalInput = !Mathf.Approximately(horizontal, 0f);
-        bool verticalInput = !Mathf.Approximately(vertical, 0f);
-
-        bool isWalking = horizontalInput || verticalInput;
+        bool isWalking = !Mathf.Approximately(vertical, 0f);
         anim.SetBool("IsWalking", isWalking);
 
-        Vector3 desireForward = Vector3.RotateTowards(transform.forward, movement, turnSpeed * Time.deltaTime, 0f);
-        rotation = Quaternion.LookRotation(desireForward);
+        // Rotación solo con horizontal
+        transform.Rotate(0f, horizontal * turnSpeed * Time.deltaTime, 0f);
+
+        if (isWalking)
+        {
+            if(!audio.isPlaying)
+            {
+                audio.Play();
+            }
+        }else
+        {
+            audio.Stop();
+        }
     }
+
+    // Update is called once per frame
+    //void Update()  //codigo antiguo
+    //{
+    //    float horizontal = Input.GetAxis("Horizontal");
+    //    float vertical = Input.GetAxis("Vertical");
+
+    //    movement.Set(horizontal, 0f, vertical);
+    //    movement.Normalize();
+
+    //    bool horizontalInput = !Mathf.Approximately(horizontal, 0f);
+    //    bool verticalInput = !Mathf.Approximately(vertical, 0f);
+
+    //    bool isWalking = horizontalInput || verticalInput;
+    //    anim.SetBool("IsWalking", isWalking);
+
+    //    Vector3 desireForward = Vector3.RotateTowards(transform.forward, movement, turnSpeed * Time.deltaTime, 0f);
+    //    rotation = Quaternion.LookRotation(desireForward);
+    //}
 
     private void OnAnimatorMove()
     {
-        rigid.MovePosition(rigid.position + movement * anim.deltaPosition.magnitude);
-        rigid.MoveRotation(rotation);
+        //rigid.MovePosition(rigid.position + movement * anim.deltaPosition.magnitude); //esto hacia que mi movimiento fuera con cordenadas globales
+        //con el siguiente codigo pasa a ser de manera local
+
+        Vector3 localMovement = transform.TransformDirection(movement); //codigo nuevo
+        rigid.MovePosition(rigid.position +  localMovement*anim.deltaPosition.magnitude); //codigo nuevo 
+        //tuve que subir el turn speed de 20f a 100f 
+
+        //rigid.MoveRotation(rotation); //codigo antiguo
     }
 }
